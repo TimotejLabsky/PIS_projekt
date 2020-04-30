86ceeaf3f2ee15c01a1488702314358ffcd20b00
Timotej Labsky <tlabsky@gmail.com>
2020-04-30 22:55:40 +0200

    adverisment picking base layout done

--- frontend/src/app/advertisement-picking/advertisement-picking.component.ts
+++ frontend/src/app/advertisement-picking/advertisement-picking.component.ts
@@ -8,6 +8,8 @@ import {ProductPriceUpdate} from "../model/product_price_update-model";
 })
 export class AdvertisementPickingComponent implements OnInit {
   columns: string[] = ['name', 'old_price', 'sales', 'delta_sales', 'include'];
+  //TODO date from to
+  actual_season: any = "20.20.2020-25.20.2020";
 
   dataSource: ProductPriceUpdate[] = [
     {name: 'te', old_price: 5, sales: 500, delta_sales: 25, new_price:5},

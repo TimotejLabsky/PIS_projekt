56fc345bdeb4e99f339cbf92fc21ba1d6749f697
Kirill Kuznecov <kirill.kuznecov@continuity.sk>
2020-05-02 20:47:22 +0200

    App settings updated

--- backend/Pis.Projekt/Business/Notifications/NotificationConfiguration.cs
+++ backend/Pis.Projekt/Business/Notifications/NotificationConfiguration.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace Pis.Projekt.Business.Notifications
 {
     public class NotificationConfiguration
--- backend/Pis.Projekt/Framework/WsdlConfiguration.cs
+++ backend/Pis.Projekt/Framework/WsdlConfiguration.cs
@@ -1,9 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Pis.Projekt.Framework
 {
     /// <typeparam name="TService">Type of Wsdl Service that uses this configuration</typeparam>
     public class WsdlConfiguration<TService>
     {
+        [NotNull]
         public string TeamId { get; set; }
+        [NotNull]
         public string Password { get; set; }
     }
 }

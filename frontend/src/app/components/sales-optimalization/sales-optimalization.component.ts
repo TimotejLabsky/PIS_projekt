a34e7f166a209e3f96726a83b05483bf7aa90d99
Timotej Labsky <tlabsky@gmail.com>
2020-05-10 19:30:29 +0200

    auto task reload on no task

--- frontend/src/app/components/sales-optimalization/sales-optimalization.component.ts
+++ frontend/src/app/components/sales-optimalization/sales-optimalization.component.ts
@@ -6,6 +6,7 @@ import {TaskService} from "../../services/task.service";
 import {AuthStore} from "../../store/auth.store";
 import {Task, TaskType} from 'src/app/model/task-model';
 import {TaskStore} from "../../store/task.store";
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,7 @@ import {TaskStore} from "../../store/task.store";
 export class SalesOptimalizationComponent implements OnInit {
   loading: boolean;
   private task: Task;
+  private updateSubscription: Subscription;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute,
               private authStore: AuthStore, private taskService: TaskService,
@@ -29,18 +31,12 @@ export class SalesOptimalizationComponent implements OnInit {
       error => console.error(error),
     )
 
-    /*let tmpTask: Task;
-    taskService.getTask(authStore.getCurrentUser()).subscribe(
-      task => tmpTask = task,
-      err => console.error(err),
-      () => this.initComplete(tmpTask)
-    );*/
-
-
   }
 
   ngOnInit(): void {
-
+    this.updateSubscription = interval(2000).subscribe(
+      (val) => { this.taskStore.loadTask()
+      });
   }
 
   private initComplete(task: Task){

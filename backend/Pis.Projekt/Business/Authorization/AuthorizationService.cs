bdbdbe082aac3203febb30030189943ebbe659b3
Kirill Kuznecov <kirill.kuznecov@continuity.sk>
2020-05-10 18:39:13 +0200

    Store authorization added

--- backend/Pis.Projekt/Business/Authorization/AuthorizationService.cs
+++ backend/Pis.Projekt/Business/Authorization/AuthorizationService.cs
@@ -19,6 +19,11 @@ namespace Pis.Projekt.Business.Authorization
 
         public async Task<bool> LoginAsync(string id, string password)
         {
+            if (AuthorizeStore(id, password))
+            {
+                return true;
+            }
+            
             var res = await _client.checkPasswordAsync(id, password)
                 .ConfigureAwait(false);
             if (!res.exists)
@@ -27,6 +32,19 @@ namespace Pis.Projekt.Business.Authorization
             }
             _logger.LogDebug($"Login of user with id: {id} was successful");
             return true;
+        }
+
+        private bool AuthorizeStore(string id, string password)
+        {
+            if (id == "predajna1" || id == "predajna2" || id == "predajna3")
+            {
+                if (password == "root")
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
 

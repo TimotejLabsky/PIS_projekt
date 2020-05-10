4ba257cb70fcecdf4a33e5968e7fa5cc79e5a166
Timotej Labsky <tlabsky@gmail.com>
2020-05-10 22:42:36 +0200

    fix task store loading

--- frontend/src/app/components/BP2/include-to-season/include-to-season.component.ts
+++ frontend/src/app/components/BP2/include-to-season/include-to-season.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {TaskService} from "../../../services/task.service";
 import {AuthStore} from "../../../store/auth.store";
 import {Product} from "../../../model/product-model";
+import {SelectionModel} from "@angular/cdk/collections";
+import { Task } from 'src/app/model/task-model';
+import {TaskStore} from "../../../store/task.store";
 
 
 @Component({
@@ -10,20 +13,38 @@ import {Product} from "../../../model/product-model";
   styleUrls: ['./include-to-season.component.css']
 })
 export class IncludeToSeasonComponent implements OnInit {
+  columns: string[] = ['name', 'old_price', 'sales', 'delta_sales', 'include'];
+
   actual_season: any;
   dataSource: Product[];
-  columns: string[] = ['name', 'old_price', 'sales', 'delta_sales', 'include'];
+  private task: Task;
   loading: boolean;
+  selection = new SelectionModel<Product>(true, []);
 
-  constructor(private taskService: TaskService, private authStore: AuthStore) {
+
+  constructor(private taskStore: TaskStore, private authStore: AuthStore) {
     this.loading = true;
-    this.taskService.getTask(authStore.getCurrentUser()).subscribe(
-      task => this.dataSource = task.products,
-      err => console.error(err),
-      () => this.loading = false
-    );
+    this.initComplete(this.taskStore.getTask());
+  }
+
+  private initComplete(task: Task){
+    this.task = task;
+    this.dataSource = this.task.products;
+    this.loading = false;
   }
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    this.task.products = this.dataSource;
+
+    this.task.products.forEach(product => (this.selection.selected.filter((item) => item == product)).length > 0
+      ? product.cancel_ordering = true : product.cancel_ordering = false);
+
+    this.taskService.fulfillTask(this.task).subscribe(
+      value => console.log(value),
+      error => console.error(error),
+      () => this.router.navigate(['sales-optimalization'])
+    );
+  }
 }

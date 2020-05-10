f4c6e462a590b4ccb8118d51c7c950d8203fc1c8
Timotej Labsky <tlabsky@gmail.com>
2020-05-10 21:47:25 +0200

    check box fix - data propagation

--- frontend/src/app/components/BP1/advertisement-picking/advertisement-picking.component.ts
+++ frontend/src/app/components/BP1/advertisement-picking/advertisement-picking.component.ts
@@ -7,6 +7,7 @@ import {combineAll} from "rxjs/operators";
 import {Router} from "@angular/router";
 import { Task } from 'src/app/model/task-model';
 import {TaskStore} from "../../../store/task.store";
+import {SelectionModel} from "@angular/cdk/collections";
 
 
 @Component({
@@ -19,6 +20,8 @@ export class AdvertisementPickingComponent implements OnInit {
   dataSource: MatTableDataSource<Product>
   actual_season: string = "20.20.2020-25.20.2020";
   loading: boolean;
+
+  selection = new SelectionModel<Product>(true, []);
 
   private task: Task = null;
 
@@ -47,12 +50,14 @@ export class AdvertisementPickingComponent implements OnInit {
 
   onSubmit() {
     this.task.products = this.dataSource.data;
-    console.log('submit')
-    console.log(this.dataSource.data);
-    /*this.taskService.fulfillTask(this.task).subscribe(
+
+    this.task.products.forEach(product => (this.selection.selected.filter((item) => item == product)).length > 0
+                                            ? product.include_to_ad = true : product.include_to_ad = false);
+    this.taskService.fulfillTask(this.task).subscribe(
       value => console.log(value),
       error => console.error(error),
       () => this.router.navigate(['sales-optimalization'])
-    );*/
+    );
   }
+
 }
--- frontend/src/app/components/BP1/ordering-cancelation/ordering-cancelation.component.ts
+++ frontend/src/app/components/BP1/ordering-cancelation/ordering-cancelation.component.ts
@@ -6,6 +6,7 @@ import {AuthStore} from "../../../store/auth.store";
 import { Task } from 'src/app/model/task-model';
 import {TaskLogger} from "protractor/built/taskLogger";
 import {Route, Router} from "@angular/router";
+import {SelectionModel} from "@angular/cdk/collections";
 
 @Component({
   selector: 'app-ordering-cancelation',
@@ -16,6 +17,8 @@ export class OrderingCancelationComponent implements OnInit {
   columns: string[] = ['name', 'cancel'];
   //TODO date from to
   actual_season: any = "20.20.2020-25.20.2020";
+
+  selection = new SelectionModel<Product>(true, []);
 
   private task: Task;
   dataSource: Product[];
@@ -41,11 +44,14 @@ export class OrderingCancelationComponent implements OnInit {
 
   onSubmit(){
     this.task.products = this.dataSource;
-    console.log('submit')
+
+    this.task.products.forEach(product => (this.selection.selected.filter((item) => item == product)).length > 0
+                                        ? product.cancel_ordering = true : product.cancel_ordering = false);
+
     this.taskService.fulfillTask(this.task).subscribe(
-     value => console.log(value),
-     error => console.error(error),
-     () => this.router.navigate(['sales-optimalization'])
+      value => console.log(value),
+      error => console.error(error),
+      () => this.router.navigate(['sales-optimalization'])
     );
   }
 
